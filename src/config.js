504ded315a345d6665bf87a43be7efaa46a7dc2e
Denzelm2005 <denzelm645@gmail.com>
2025-04-11 15:49:38 -0600

    Se actualiza

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ config();
 export const PORT = process.env.PORT || 3000;
 export const DB_HOST = process.env.DB_HOST = 'localhost';
 export const DB_PORT = process.env.DB_PORT || 3306;
-export const DB_USER = process.env.DB_USER || 'luiscastilla';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'luiscastilla';
+export const DB_USER = process.env.DB_USER || 'DenzelMarin';
+export const DB_PASSWORD = process.env.DB_PASSWORD || '123456';
 export const DB_DATABASE = process.env.DB_DATABASE || 'moto_repuestos_leyton_1';
